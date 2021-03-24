96a46a6cf324a339d8a7de05e627027e8a37ee07
Niko <76891277+nikoap92@users.noreply.github.com>
2021-03-24 02:12:05 -0400

    Update PacMan.js

--- PacMan.js
+++ PacMan.js
@@ -80,7 +80,3 @@ function makeOne() {
   pacMen.push(makePac()); // add a new PacMan
 }
 
-//don't change this line
-// if (typeof module !== 'undefined') {
-//   module.exports = { checkCollisions, update, pacMen };
-// }
